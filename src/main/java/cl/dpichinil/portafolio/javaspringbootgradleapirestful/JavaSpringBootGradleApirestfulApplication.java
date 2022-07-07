d177f04f7f3cccee34f2b631d61279a627413766
Daniel Pichinil <daniel.pichinil@gmail.com>
2022-07-07 07:10:49 -0400

    seagrega version inicial

---
+++ src/main/java/cl/dpichinil/portafolio/javaspringbootgradleapirestful/JavaSpringBootGradleApirestfulApplication.java
@@ -0,0 +1,13 @@
+package cl.dpichinil.portafolio.javaspringbootgradleapirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootGradleApirestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootGradleApirestfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/dpichinil/portafolio/javaspringbootgradleapirestful/JavaSpringBootGradleApirestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.dpichinil.portafolio.javaspringbootgradleapirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootGradleApirestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
